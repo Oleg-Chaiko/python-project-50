6155e2ae7e74853753c15f65ec29ead28c7de84e
Oleg <chaiko.oleg89@gmail.com>
2024-02-16 13:36:29 +0300

    fix:bug fixes

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -13,15 +13,10 @@ def plain(diff, current_key=''):
         status = v['status']
         match status:
             case 'added':
-                if isinstance(v['value'], dict):
-                    result.append(
-                        f"Property '{current_key}{k}'\
- was added with value: [complex value]"
-                    )
-                else:
-                    result.append(
-                        f"Property '{current_key}{k}'\
- was added with value: '{v['value']}'"
+                val = format_string(v['value'])
+                result.append(
+                    f"Property '{current_key}{k}'\
+ was added with value: {val}"
                     )
             case 'deleted':
                 result.append(f"Property '{current_key}{k}' was removed")
@@ -38,6 +33,6 @@ def plain(diff, current_key=''):
                     val = v['value']
                     result.append(plain(val, cur_key))
     return '\n'.join(result)\
-        .replace(" 'False'", " false")\
-        .replace(" 'True'", " true")\
-        .replace(" 'None'", " null")
+        .replace(" False", " false")\
+        .replace(" True", " true")\
+        .replace(" None", " null")
